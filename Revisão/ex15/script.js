9fa59bbf5f90964ea71392b21689d44741c10883
MarcosB4Silva <marcosbs252@gmail.com>
2023-07-21 17:19:50 -0300

    array 21/07/23

---
+++ Revisão/ex15/script.js
@@ -0,0 +1,21 @@
+/* Crie uma lista com 5 itens
+    altere o 3° e o 5° item
+    adicione um 6° item 
+
+    crie uma lista com dois itens
+    unir ambas as listas
+*/
+
+
+let l1 = ['Jheff','Olavo','Bolsonaro','Lula','Ovo'];
+
+l1[1] = 'Jão'
+l1[4] = 'Maria'
+
+l1.push('38')
+
+let l2 = ['masqueico','mamaco']
+
+l1.concat(l2)
+
+console.log(l1)
